f81b73305a1ae21673f59da4efc559a160fef5fd
ThraexDev <peter.truckenbrod@t-online.de>
2018-07-21 00:42:33 +0200

    changed page tracking

--- routes/pages.js
+++ routes/pages.js
@@ -1,30 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function (req, res, next) {
-    if (req.method == "GET") {
-        req.visitor.pageview(req.originalUrl).send();
-    }
-    next(); // <-- important!
-});
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    req.visitor.pageview(req.originalUrl).send();
     res.render('home');
 });
 
 router.get('/divisioncalculator', function(req, res, next) {
+    req.visitor.pageview(req.originalUrl).send();
     res.render('divisionscreen');
 });
 
 router.get('/navycalculator', function(req, res, next) {
+    req.visitor.pageview(req.originalUrl).send();
     res.render('navyscreen');
 });
 
 router.get('/aircraftcalculator', function(req, res, next) {
+    req.visitor.pageview(req.originalUrl).send();
     res.render('aircraftscreen');
 });
 
 router.get('/legalnotice', function(req, res, next) {
+    req.visitor.pageview(req.originalUrl).send();
     res.render('legalnotice');
 });
 
