956137dc3fb696a93b135a79b0e06f10496f1d05
ThraexDev <peter.truckenbrod@t-online.de>
2018-07-24 01:54:32 +0200

    bad changes

--- routes/pages.js
+++ routes/pages.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    req.visitor.pageView(req.protocol + '://' + req.get('host') + req.originalUrl);
+    req.visitor.pageview(req.originalUrl).send();
     res.render('home');
 });
 
 router.get('/divisioncalculator', function(req, res, next) {
-    req.visitor.pageView(req.protocol + '://' + req.get('host') + req.originalUrl);
+    req.visitor.pageview(req.originalUrl).send();
     res.render('divisionscreen');
 });
 
 router.get('/navycalculator', function(req, res, next) {
-    req.visitor.pageView(req.protocol + '://' + req.get('host') + req.originalUrl);
+    req.visitor.pageview(req.originalUrl).send();
     res.render('navyscreen');
 });
 
 router.get('/aircraftcalculator', function(req, res, next) {
-    req.visitor.pageView(req.protocol + '://' + req.get('host') + req.originalUrl);
+    req.visitor.pageview(req.originalUrl).send();
     res.render('aircraftscreen');
 });
 
 router.get('/legalnotice', function(req, res, next) {
-    req.visitor.pageView(req.protocol + '://' + req.get('host') + req.originalUrl);
+    req.visitor.pageview(req.originalUrl).send();
     res.render('legalnotice');
 });
 
